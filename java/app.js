39de2555c26f37f27cf359edaf1be0c29339bfa9
tl <tl@PC14-156.njitdm.campus.njit.edu>
2017-11-11 16:09:21 -0500

    Fixed Version

---
+++ java/app.js
@@ -0,0 +1,14 @@
+// var user = prompt("What's Your Name?");
+//
+// function checkUser(tUser){
+//   if( tUser == "Dajah" ){
+//     alert("Welcome Dajah");
+//   }
+//   else if( tUser == "Justino" ){
+//     alert("Welcome Justino")
+//   }
+//   else{
+//     alert("Get Out!");
+//   }
+// }
+// checkUser(user);
